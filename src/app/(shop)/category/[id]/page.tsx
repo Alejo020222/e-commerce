80ade29b6b3ad731f305ac973a40b8045c990fb7
Alejandro Morales <ale75342mb@gmail.com>
2024-04-20 18:29:55 -0400

    create_product interface

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,21 +1,39 @@
+import { ProductGrid, Title } from "@/components";
+import { Category } from "@/interface";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
 
 interface Props {
   params: {
-    id: string;
+    id: Category;
   };
 }
 
+const seeProduct = initialData.products;
+
 export default function ({ params }: Props) {
   const { id } = params;
+  const products = seeProduct.filter((product) => product.gender === id);
 
-  if (id == "kids") {
-    notFound();
-  }
+  // if (id == "kids") {
+  //   notFound();
+  // }
+
+  const label: Record<Category, string> = {
+    men: "para hombre",
+    women: "para mujeres",
+    kid: "para niños",
+    unisex: "para todos",
+  };
 
   return (
     <div>
-      <h1>Category Page {id}</h1>
+      <Title
+        title={`Artículos ${label[id]}`}
+        subtitle="Todos los productos"
+        className=" mb-2"
+      />
+      <ProductGrid products={products} />
     </div>
   );
 }
